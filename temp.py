e9f8c7d396f4876e7f91a637e7d9bcfa3f2bf21c
aquiles021110 <aquiles@bousigues.com>
2025-05-28 15:59:59 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,11 +5,25 @@ print(v)
 
 print("Hello \t World")'''
 
-a=int(input('Choose number 1\n'))
-b=int(input('Choose number 2\n'))
-if a>b:
+
+'''if a>b:
     print(a,'+',b,f'= {a+b}')
 elif a<b:
     print(a,'-',b,f'= {a-b}')
 elif a==b:
-    print(a,'X',b,f'= {a*b}')
+    print(a,'X',b,f'= {a*b}')'''
+
+a=int(input('Choose number 1\n'))
+b=int(input('Choose number 2\n'))
+num=a if a < b else b
+#or
+'''res=a | b'''
+#and
+'''res=a & b'''
+#negation
+'''res=~a'''
+#left shift
+'''res=a << 2'''
+#right shift
+'''res=a >>2'''
+print(res)
